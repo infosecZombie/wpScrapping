26fc6f98ee3514885457ba151062cbf8e9e6da72
infosecZombie <69755943+infosecZombie@users.noreply.github.com>
2020-11-06 03:48:30 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Ws Link</title>
+        <title>Danish</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
